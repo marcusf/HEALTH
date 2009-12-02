f99580ece8fe41ea29d991df9ef7d3577b1e8db5
Marcus Frödin <marcusf@c-0b0be253.1230-1-64736c10.cust.bredbandsbolaget.se>
2009-12-03 00:42:12 +0100

    Added error counting

--- config.js
+++ config.js
@@ -1,14 +1,21 @@
+/* == HEALTH CONFIGURATION ======================================= */
+
+/* HEALTH Client configuration */
 exports.client = {
-    "delay"    : 50
+    "delay"    : 20 // Delay time in ms 
 };
 
+/* Server configuration */
 exports.host = {
-    "domain"   : "localhost",
-    "httpHost" : "localhost",
-    "port"     : 8080
+    "domain"   : "localhost", // Domain name to connect to
+    "port"     : 8080,        // Port to domain
+    "httpHost" : "localhost"  // URL to send as host parameter
 };
 
+/* List of urls under host.domain to visit */
 exports.urls = [ 
     "/",
-    "/cmlink"
+    "/cmlink",
+    "/cmlink/greenfield-times",
+    "/herre-min-varld-vad-fel"
 ];
--- storm-client.js
+++ storm-client.js
@@ -1,7 +1,6 @@
 /***
  * HEALTH
  * - Spamma urlar ur en lista
- * - Sätta maxspam
  * - Föra statistik: Request-tid, responskod
  * - Spara statistik i CouchDB
  * - Aggregera statistik
@@ -17,19 +16,26 @@ var currentTime  = function() { return new Date().getTime() };
 var printResults = function() { 
     
     var avg = 0, max = 0, min = 9999999999999, 
-        cnt = results.length, runtime = (currentTime() - startedAt)/1000;
-    
+        cnt = results.length, runtime = (currentTime() - startedAt)/1000,
+        codes = {}, errors = 0;
+        
     for (i = 0; i < cnt; i++) {
         var res = results[i];
         avg += res.requestTime;
         max  = Math.max(max, res.requestTime);
         min  = Math.min(min, res.requestTime);
+        // Status codes and error calculation
+        if (codes[res.status] == undefined) { codes[res.status] = 0; }
+        codes[res.status]++;
+        if (res.status >= 400 && res.status < 600) errors++;
     }
     
     avg /= cnt;
     
-    var throughput = parseInt(1000.0/avg);
+    var throughput = parseInt(1000.0/avg),
+        errors     = parseInt(10000*errors/cnt)/100;
     
+    sys.puts("\nHEALTH")
     sys.puts("-------------------------");
     sys.puts("Runtime:\t"        + runtime + " s");
     sys.puts("Total requests:\t" + cnt);
@@ -37,6 +43,12 @@ var printResults = function() {
     sys.puts("Average time:\t"   + parseInt(avg) + " ms");
     sys.puts("Max time:\t"       + max + " ms");
     sys.puts("Min time:\t"       + min + " ms");
+    sys.puts("Errors:\t\t"       + errors + "%");
+    
+    sys.puts("\nResponse code frequency:\t")
+    for (code in codes) {
+        sys.puts(code + ":\t\t" + codes[code] + " times");
+    }
 }
 
 var GET = function(url, cfg, results) {
